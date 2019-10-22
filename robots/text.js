19f5695272386684393c030930cd6cfcc28effdc
Francielle Luz <francielle.luz@gft.com>
2019-10-22 16:47:42 -0300

    inclusao do brek sentence

--- robots/text.js
+++ robots/text.js
@@ -1,15 +1,17 @@
 const algorithmia = require('algorithmia')
 const algorithmiaApiKey = require('../credentials/algorithmia.json').apiKey
+const sentenceBoundaryDetection = require('sbd')
  async function robot(content){
     await fetchContentFromWikipidia(content)
     sanitizeContent(content)
-    //breakContentIntoSentences(content)
+    breakContentIntoSentences(content)
    async function fetchContentFromWikipidia(content){
         const algorithmiaAuthenticated = algorithmia(algorithmiaApiKey)
         const wikipediaAlgorithim = algorithmiaAuthenticated.algo('web/WikipediaParser/0.1.2')
         const wikipediaResponse = await wikipediaAlgorithim.pipe(content.searchTerm)
         const wikipediaContent = wikipediaResponse.get()
         content.sourceContentOriginal = wikipediaContent.content
+        
     }
     function sanitizeContent(content){
         const withoutBlankLinesAndMarkdown = removeBlankLinesAndMarkdown(content.sourceContentOriginal)
@@ -28,6 +30,17 @@ const algorithmiaApiKey = require('../credentials/algorithmia.json').apiKey
         function removeDatesInParenthesis(text){
             return text.replace(/\((?:\([^()]*\)|[^()])*\)/gm, '').replace(/  /g,' ')
         }
+    }
+    function breakContentIntoSentences(content){
+        content.sentences = []
+        const sentences = sentenceBoundaryDetection.sentences(content.sourceContentSanitized)
+        sentences.forEach((sentence)=>{
+            content.sentences.push({
+                text: sentence,
+                keywords: [],
+                images: []
+            })
+        })
     }
 }
 module.exports = robot
