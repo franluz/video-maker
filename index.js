3ddd3b14062102180207a57c91412c84c565aca9
Francielle Luz <francielle.luz@gft.com>
2019-10-22 11:09:45 -0300

    inclusao do google trends para a definição de qual trend será buscada na wikipidia

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
 const readline = require('readline-sync')
+const Parser = require('rss-parser')
+const TREND_URL = 'https://trends.google.com/trends/trendingsearches/daily/rss?geo=BR' 
+    async function start(){
 
-function start(){
-    const content = {}
-    content.searchTerm = askAndReturnTerm()
-    content.prefix = askAndReturnPrefix()
-    
-    function askAndReturnTerm(){
-        return readline.question('Type a Wikipidia search Term: ')
-    }
-    function askAndReturnPrefix(){
-        const prefixes = ['Who is', 'What is ', 'The History of']
-        const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Choose one option: ')
-        const selectedPrefixText = prefixes[selectedPrefixIndex]
-        return selectedPrefixText
+        const content = {}
+        content.searchTerm = await askAndReturnTerm()
+        content.prefix =  askAndReturnPrefix()
         
+        async function askAndReturnTerm(){
+            const response = readline.question('Type a Wikipidia search Term: ')
+            return (response.toUpperCase() === 'G') ? await askAndReturnTrend(): response
+        }
+        async function askAndReturnTrend(){
+            console.log('Please Wait...')
+            const trends = await  getGoogleTrends()
+            const choice = readline.keyInSelect(trends, 'Chooice your trend:')
+            return trends[choice]
+        }
+        async function getGoogleTrends(){
+            const parser = new Parser();
+            const trends = await parser.parseURL(TREND_URL);
+            return trends.items.map(i => i.title)
+        }
+        function askAndReturnPrefix(){
+            const prefixes = ['Who is', 'What is ', 'The History of']
+            const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Choose one option: ')
+            const selectedPrefixText = prefixes[selectedPrefixIndex]
+            return selectedPrefixText
+            
+        }
+        console.log(content)
     }
-    console.log(content)
-}
 start();
