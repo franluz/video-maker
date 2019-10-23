b55a4a4b70e5678269a7d6b1352893f43aa4b433
Francielle Luz <francielle.luz@gft.com>
2019-10-23 15:39:29 -0300

    implementação do watson

--- robots/text.js
+++ robots/text.js
@@ -1,10 +1,22 @@
 const algorithmia = require('algorithmia')
 const algorithmiaApiKey = require('../credentials/algorithmia.json').apiKey
+const watsonApiKey = require('../credentials/watson-nlu.json')
 const sentenceBoundaryDetection = require('sbd')
+const NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-language-understanding/v1.js')
+ 
+const nlu = new NaturalLanguageUnderstandingV1({
+    iam_apikey: watsonApiKey.apikey,
+    version: '2018-11-16',
+    url: watsonApiKey.url
+});
+
  async function robot(content){
     await fetchContentFromWikipidia(content)
     sanitizeContent(content)
     breakContentIntoSentences(content)
+    limitMaximunSentences(content)
+    await fetchKeywordsOfAllSentences(content)
+
    async function fetchContentFromWikipidia(content){
         const algorithmiaAuthenticated = algorithmia(algorithmiaApiKey)
         const wikipediaAlgorithim = algorithmiaAuthenticated.algo('web/WikipediaParser/0.1.2')
@@ -42,5 +54,34 @@ const sentenceBoundaryDetection = require('sbd')
             })
         })
     }
+    async function fetchKeywordsOfAllSentences(content) {
+        for(const sentence of content.sentences){
+            sentence.keywords = await fetchWatsonAndReturnKeywords(sentence.text)
+        }
+    }
+    async function fetchWatsonAndReturnKeywords(sentence) {
+        return new Promise((resolve, reject) => {
+            nlu.analyze({
+                text: sentence,
+                features: {
+                    keywords: {}
+                }
+            }, (error, response) => {
+                if(error) {
+                    throw error
+                }
+            
+                const keywords = response.keywords.map((keyword) => {
+                    return keyword.text
+                })
+    
+                resolve(keywords)
+            })
+        })
+    }
+    function limitMaximunSentences(content){
+        content.sentences = content.sentences.slice(0,content.maximumSentences)
+    }
+         
 }
 module.exports = robot
